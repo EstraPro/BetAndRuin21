b6aaad1464fd6fe1c32cf62fc42ffaaacf4074ab
estrapro <estrapro@estrapro-pc>
2021-03-01 20:30:08 +0100

    User class added

---
+++ BetAndRuin 21/src/domain/User.java
@@ -0,0 +1,46 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	private int id;
+	private String username;
+	private String password;
+	
+	public User(int id, String usr, String passwd) {
+		
+		this.id = id;
+		username = usr;
+		password = passwd;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
