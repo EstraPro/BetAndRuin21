aa4449c6d135b9c8dd46bfd4c2b81c5be7df5f36
estrapro <estrapro@estrapro-pc>
2021-03-02 16:56:56 +0100

    Correct credentials bug fixed

--- BetAndRuin 21/src/businessLogic/PassChecker.java
+++ BetAndRuin 21/src/businessLogic/PassChecker.java
@@ -2,7 +2,7 @@ package businessLogic;
 
 public class PassChecker {
 	public boolean checkCredentialsUser(String usname, String passwd) {
-		if (usname=="user" && passwd=="user") {
+		if (usname.equals("user") && passwd.equals("user")) {
 			return true;
 		}else {
 			return false;
@@ -10,7 +10,7 @@ public class PassChecker {
 	}
 	
 	public boolean checkCredentialsAdmin(String usname, String passwd) {
-		if (usname=="administrator" && passwd=="administrator") {
+		if (usname.equals("administrator") && passwd.equals("administrator")) {
 			return true;
 		}else {
 			return false;
